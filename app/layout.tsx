37c21cc1f175a043c22766169ad96510f5bd8187
Joonas <jos00117@laurea.fi>
2026-05-10 00:01:21 +0300

    error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
+import '@/app/ui/global.css';
+
  
 export const metadata: Metadata = {
   title: {
