7ac16ab503d0d81c72c9dcd6f6e3f58012d9661f
Joonas <jos00117@laurea.fi>
2026-05-10 00:06:27 +0300

    muutos

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { CustomerField } from '@/app/lib/definitions';
+import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
   CheckIcon,
